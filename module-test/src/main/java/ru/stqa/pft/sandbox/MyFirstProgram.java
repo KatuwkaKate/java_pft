84419d98af2bf19366fecb1e92bdef2c7f736272
KatuwkaKate <34887069+KatuwkaKate@users.noreply.github.com>
2018-01-09 15:55:26 +0300

    знаки действий

---
+++ module-test/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 3);
+        System.out.println(2 + 7);
+        System.out.println(3 - 2);
+        System.out.println(25 / 5);
+
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+
+
+
+    }
+
+}

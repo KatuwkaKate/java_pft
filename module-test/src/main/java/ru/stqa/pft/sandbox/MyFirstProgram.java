fdb57f171e4b87eb28f725901b17f0c70c2482ac
KatuwkaKate <34887069+KatuwkaKate@users.noreply.github.com>
2018-01-17 15:43:42 +0300

    обавлено несколько функций

--- module-test/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ module-test/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,17 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String somebody = "world";
+        hello ("world!");
+        hello ("user");
+        hello ("Ekaterina");
+
+        double l = 5;
+        System.out.println ("Площадь квадрата со стороной" + l + "=" + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println ("Площадь прямоугольника со сторонами" + a + "и" + b + "=" + area(a,b));
 
         System.out.println(2 + 2);
         System.out.println(2 * 3);
@@ -17,8 +27,18 @@ public class MyFirstProgram {
         System.out.println("2" + "2");
         System.out.println("2" + 2);
 
-
-
+        
     }
 
+    public static void hello (String somebody) {
+        System.out.println("Hello," + somebody + "!");
+    }
+
+    public static double area (double len) {
+        return len * len;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
+    }
 }
